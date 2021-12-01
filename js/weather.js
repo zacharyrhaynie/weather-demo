29ffeb5ed189c8b8f9927514864416578668e137
tipjoe <joe.tippetts@lendio.com>
2021-12-01 16:13:18 -0700

    Remove unneeded dependencies and change to new weather api -- other one died.

--- js/weather.js
+++ js/weather.js
@@ -1,30 +1,35 @@
-$(function() {
-  // Enable autocomplete on the search field.
-  $("input.typeahead").typeahead({
-    onSelect: function(item) {
-      // Clear previous weather results, if any.
-      clearResult();
-      // Get the weather info for the selected search location.
-      $.get( "/weather.php?query=" + item.value, function(data) {
-        // Show weather results.
+$(function () {
+  // React to hitting enter in the text box instead of clicking submit.
+  $("#search-weather").submit(function(e) {
+    e.preventDefault();
+    clearResult('');
+
+    // Get the weather info for the selected search location.
+    $.get("/weather.php?query=" + $("#search").val(), function (data) {
+      // Show weather results.
+      if (data.temp_f) {
         showResult(data);
-      });
-    },
-    ajax: {
-      url: 'weather.php?autocomplete=1',
-      timeout: 200,
-      displayField: "name",
-    }
-  })
+      } else {
+        clearResult('No valid data for your location.');
+      }
+    });
+  });
+
+  function clearResult($msg = 'Sorry Charlie! Not a valid input.') {
+    $(".weather_icon").attr("src", "images/trans.png");
+    $("#location").hide();
+    $("#city").text();
+    $("#state").text();
+    $(".result .description").html($msg);
+  }
+
+  function showResult(data) {
+    $(".weather_icon").attr("src", data.icon_url);
+    $("#location").show();
+    $("#city").text(data.city);
+    $("#state").text(data.state);
+    var desc = data.weather + " and " + data.temp_f + "&deg; F";
+    $(".result .description").html(desc);
+  }
+
 });
-
-function clearResult() {
-  $(".weather_icon").attr("src", "images/trans.png");
-  $(".result .description").html("");
-}
-
-function showResult(data) {
-  $(".weather_icon").attr("src", data.icon_url);
-  var desc = data.weather + " and " + data.temp_f + "&deg; F";
-  $(".result .description").html(desc);
-}
--- weather.php
+++ weather.php
@@ -1,40 +1,30 @@
 <?php
   /**
-   * This page talks to weather.com's API (wunderground).
-   * It either returns a list of  autocomplete locations based
-   * on user input or returns the selected weather result.
+   * This page talks to weatherapi.com.
    */
 
+  // API URL.
+  // You can register for free to get your own api key (fb0... below). 
+  $url = "http://api.weatherapi.com/v1/current.json?key=fb0b4c5c22d04c22be2202032210112&aqi=no&q=";
+
   // Check and filter query parameters.
-  $autocomplete = filter_input(INPUT_GET, 'autocomplete', FILTER_DEFAULT);
-  $q = filter_input(INPUT_GET, 'query', FILTER_DEFAULT);
+  $query = filter_input(INPUT_GET, 'query', FILTER_DEFAULT);
 
-  if (isset($autocomplete) && isset($q) && $autocomplete == 1) {
-    $q = urlencode($q);
-    $data = file_get_contents("http://autocomplete.wunderground.com/aq?query=" . $q);
-    $parsed_data = json_decode($data);
+  if ($query) {
+    $query = urlencode($query);
 
-    foreach ($parsed_data->RESULTS as $loc) {
-      // Lat/Lon is the unique ID for each location.
-      $latlon = $loc->lat . ',' . $loc->lon;
-      $result[] = array('id' => $latlon, 'name' => $loc->name);
-    }
-
-    if (!isset($result)) {
-      $result[] = array('id' => -1, 'name' => $q . ' not found');
-    }
-
-    header('Content-Type: application/json');
-    echo json_encode($result);
-
-  } elseif (isset($q)) {
-    // Return weather info for chosen location.
-    $data = file_get_contents("http://api.wunderground.com/api/a57f820f5f3e7a5d/geolookup/conditions/q/" . $q . ".json");
+    // Call the api with the user input query (zip, city, etc.).
+    $data = file_get_contents($url . $query);
     $parsed = json_decode($data);
-    $result['weather'] = $parsed->current_observation->weather;
-    $result['temp_f'] = $parsed->current_observation->temp_f;
-    $result['icon_url'] = $parsed->current_observation->icon_url;
+
+    // You can use PHP's error_log to output a string to the log for debugging.
+    // error_log($parsed->location->name);
+
+    $result['weather'] = $parsed->current->condition->text;
+    $result['temp_f'] = $parsed->current->temp_f;
+    $result['icon_url'] = $parsed->current->condition->icon;
+    $result['city'] = $parsed->location->name;
+    $result['state'] = $parsed->location->region;
     header('Content-Type: application/json');
     echo json_encode($result);
   }
-?>
